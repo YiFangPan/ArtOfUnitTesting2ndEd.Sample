4f7bfb8a692a2598a1ce477a09c6f45c4a6b7b17
YiFangPan <sber13@gmail.com>
2020-05-01 23:37:18 +0800

    Testing and verify system's status change III
    1. Test add method and verify total value
    * chapter 2.7 - program 2-8

--- LogAn.UnitTests/MemCalculatorTests.cs
+++ LogAn.UnitTests/MemCalculatorTests.cs
@@ -4,11 +4,26 @@ namespace LogAn.UnitTests
 {
     public class MemCalculatorTests
     {
+        [Test]
         public void sum_ByDefault_ReturnsZero()
         {
-            MemCalculator memCalculator = new MemCalculator();
+            MemCalculator memCalculator = getMemCalculator();
             int lastTotal = memCalculator.sum();
             Assert.AreEqual(0, lastTotal);
+        }
+
+        [Test]
+        public void add_WhenCalled_ChangesSum()
+        {
+            MemCalculator memCalculator = getMemCalculator();
+            memCalculator.add(1);
+            int total = memCalculator.sum();
+            Assert.AreEqual(1, total);
+        }
+
+        private MemCalculator getMemCalculator()
+        {
+            return new MemCalculator();
         }
     }
 }
